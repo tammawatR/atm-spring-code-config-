aff1616c6c2441bd8f32c91d4ecc1b9d285a903e
tammawatR <48899651+tammawatR@users.noreply.github.com>
2020-09-14 20:44:12 +0700

    Add files via upload

---
+++ atm-spring-code-config/src/main/java/th/ac/ku/atm/ATMConfig.java
@@ -0,0 +1,22 @@
+package th.ac.ku.atm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ATMConfig {
+    @Bean
+    public  DataSource dataSource(){
+        return new DataSourceFile();    //DB ,File ,WebAPI เลือกได้
+    }
+
+    @Bean
+    public  Bank bank(){
+        return new Bank("My Bank",dataSource());
+    }
+
+    @Bean
+    public  ATM atm(){
+        return new ATM(bank());
+    }
+}
